a2a7cc5b1b78f3cb1d1c2ce8e7bcadd04f14c8b7
dias <diassb@mail.ru>
2024-03-02 16:05:43 +0500

    Version 3

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("TEST 1");
         System.out.println("TEST 2");
-
+        System.out.println("TEST 3");
     }
 }
