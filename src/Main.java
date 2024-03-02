5eb4578a619c208c92adbc87dfa0bfeeff90cfb0
dias <diassb@mail.ru>
2024-03-02 16:11:26 +0500

    Version 4

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("TEST 1");
-        System.out.println("TEST 2");
-        System.out.println("TEST 3");
+        System.out.println("Version 4");
     }
 }
